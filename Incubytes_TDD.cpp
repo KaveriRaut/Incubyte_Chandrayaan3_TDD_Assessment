e4ddbc31bd90b3a634169cdc81972c7d3b821163
KaveriRaut <rautkaveri02@gmail.com>
2023-09-14 21:24:16 +0530

    added unit tests for turnLeft function and tested it

--- Incubytes_TDD.cpp
+++ Incubytes_TDD.cpp
@@ -87,6 +87,35 @@ class Spacecraft
             }
         }
 
+        // Function to turn the spacecraft left (based on spaceCraft's left hand)
+        void turnLeft()
+        {
+            if (direction == 'N')
+            {
+                direction = 'W';
+            }
+            else if (direction == 'S')
+            {
+                direction = 'E';
+            }
+            else if (direction == 'E')
+            {
+                direction = 'N';
+            }
+            else if (direction == 'W')
+            {
+                direction = 'S';
+            }
+            else if (direction == 'U')
+            {
+                direction = 'N'; // Left turn while facing up becomes North
+            }
+            else if (direction == 'D')
+            {
+                direction = 'S'; // Left turn while facing down becomes South
+            }
+        }
+
         // Function to get the current position and direction
         void getCurrentPositionAndDirection()
         {
@@ -120,15 +149,27 @@ void runTests()
     
     // Tests for MoveBackword operation ----------------------------------------------------------
 
-    // Test 1: Move forward when facing North
+    // Test 1: Move backward when facing North
     chandrayaan3.moveBackward();
     cout<<"moveBackward: ";
     chandrayaan3.getCurrentPositionAndDirection(); // Expected: Position: (0, 1, 0), Direction: N
 
-    // Test 2: Move forward again when facing North 
+    // Test 2: Move backward again when facing North 
     chandrayaan3.moveBackward();
     cout<<"moveBackward: ";
     chandrayaan3.getCurrentPositionAndDirection(); // Expected: Position: (0, 0, 0), Direction: N
+    
+    // Tests for TurnLeft operation ----------------------------------------------------------
+
+    // Test 1: Turn Left when facing North
+    chandrayaan3.turnLeft();
+    cout<<"turnLeft: ";
+    chandrayaan3.getCurrentPositionAndDirection(); // Expected: Position: (0, 0, 0), Direction: W
+
+    // Test 2: Turn Left again when facing North 
+    chandrayaan3.turnLeft();
+    cout<<"turnLeft: ";
+    chandrayaan3.getCurrentPositionAndDirection(); // Expected: Position: (0, 0, 0), Direction: S
 }
 
 int main()
